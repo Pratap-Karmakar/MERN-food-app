5b7ccb76dc13485672c92b47a6079ca2104bf053
Pratap Karmakar <pratap2501.pk@gmail.com>
2023-05-13 19:34:51 +0530

    implemented react-router-dom

---
+++ src/screens/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import { Footer } from '../components/Footer'
+
+export const Home = () => {
+  return (
+    <div>
+        <div><Navbar/></div>
+        <div>Body</div>
+        <Footer/>
+    </div>
+  )
+}
